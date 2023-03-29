45def1a9074cf98182ae80fdc3b6577cbfee0913
KBCHANDANA506 <chandanasri888@gmail.com>
2023-03-29 14:36:14 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def parent1(request):
+    return render(request,'parent1.html')
+
+def child1(request):
+    return render(request,'child1.html')
+
+def parent2(request):
+    return render(request,'parent2.html')
+
+def child2(request):
+    return render(request,'child2.html')
